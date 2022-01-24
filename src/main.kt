68d6c15cd570f3474039e45a169b5f5f2b68aff4
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-24 19:36:29 -0300

    uso dos lacos de repeticao

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,46 @@
 fun main() {
     println("Bem vindo ao Bytebank");
-    val titular = "Alex";
-    val numeroConta = 1000;
-    var saldo = 0.0;
 
-    saldo = 100 + 2.0;
-    saldo += 200;
-    saldo = -1.0;
+    // conta de 1 até 5
+    for (i in 1..5) {
+        val titular = "Alex $i";
+        val numeroConta = 1000 + i;
+        var saldo = i + 10.0;
 
-    println("titular: $titular");
-    println("numero da conta $numeroConta");
-    println("saldo da conta $saldo");
+        println("titular: $titular");
+        println("numero da conta $numeroConta");
+        println("saldo da conta $saldo");
+        println();
+    }
 
+    // conta de 5 até 1 saltando de 2 em 2
+    for (i in 5 downTo 1 step 2) {
+        val titular = "Mateus $i";
+        val numeroConta = 2000 + i;
+        var saldo = i + 20.0;
+
+        println("titular: $titular");
+        println("numero da conta $numeroConta");
+        println("saldo da conta $saldo");
+        println();
+    }
+
+    // conta de 1 até 4 (exclui o ultimo valor da itereacao)
+    for (i in 1 until 5) {
+        val titular = "Marquinhos $i";
+        val numeroConta = 3000 + i;
+        var saldo = i + 30.0;
+
+        println("titular: $titular");
+        println("numero da conta $numeroConta");
+        println("saldo da conta $saldo");
+        println();
+    }
+//    testaCondicoes(saldo);
+
+}
+
+fun testaCondicoes(saldo: Double) {
     if(saldo > 0.0){
         println("conta é positiva")
     } else if (saldo == 0.0){
