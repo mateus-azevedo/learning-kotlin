b369c893aa379254737c4491f9d0ee9ff285f259
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-25 21:30:52 -0300

    implementando o gerente no projeto

--- bytebank/src/Funcionario.kt
+++ bytebank/src/Funcionario.kt
@@ -1,15 +1,10 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double,
-    val tipo: Int // 0 funcionario, 1 gerente, 2 diretor
+    val salario: Double
 ) {
     fun bonificacao(): Double {
-        return when (tipo) {
-            0 -> salario * 0.1
-            1 -> salario * 0.2
-            else -> salario * 0.3
-        }
+        return salario * 0.1;
     }
 
 }
--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,17 +1,33 @@
 fun main() {
-    println("Bem vindo ao Bytebank");
+    println("Bem vindo ao Bytebank\n");
 
     val alex = Funcionario(
         nome = "Alex",
         cpf = "111.111.111-11",
-        salario= 1000.0,
-        tipo = 0
+        salario= 1000.0
     )
 
     println("nome ${alex.nome}")
     println("cpf ${alex.cpf}")
     println("salario ${alex.salario}")
-    println("bonificação ${alex.bonificacao()}")
+    println("bonificação ${alex.bonificacao()}\n")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("nome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("bonificação ${fran.bonificacao()}")
+
+    if (fran.autentica(senha = 1234))
+        println("autenticou com sucesso")
+    else
+        println("falha na autenticação")
 }
 
 
