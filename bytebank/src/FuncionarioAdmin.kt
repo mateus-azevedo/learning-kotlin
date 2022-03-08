304065fa2ad9323676633e2d68dc4f71541e9aff
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-08 14:26:27 -0300

    implementado FuncionarioAdmin - codigo quebrado

---
+++ bytebank/src/FuncionarioAdmin.kt
@@ -0,0 +1,16 @@
+abstract class FuncionarioAdmin(
+    val nome: String,
+    val cpf: String,
+    val salario: Double,
+    val senha: Int
+) {
+    abstract val bonificacao: Double
+
+    fun autentica(senha: Int): Boolean {
+        if(this.senha == senha) {
+            return true
+        }
+
+        return false
+    }
+}
--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,6 +1,20 @@
 fun main() {
-    println("Bem vindo ao Bytebank\n");
+    val gerente = Gerente(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0,
+        senha = 1000
+    )
 
-    testaContasDiferentes()
+    val diretora = Diretor(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 2000,
+        plr = 200.0
+    )
+
+    val sistema = SistemaInterno()
+    sistema.entra(gerente, 1000)
+    sistema.entra(diretora, 2000)
 }
-
