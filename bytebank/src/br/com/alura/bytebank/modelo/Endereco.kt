5cff6f8b6f1ba93f54489f910079c5f1567a1a8d
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-15 12:36:05 -0300

    sobrescrevendo os metodos hashCode() e equals() - uso do intellij (alt + insert)

--- bytebank/src/br/com/alura/bytebank/modelo/Endereco.kt
+++ bytebank/src/br/com/alura/bytebank/modelo/Endereco.kt
@@ -22,13 +22,32 @@ class Endereco(
     }
 
     override fun equals(other: Any?): Boolean {
-        if (other != null && other is Endereco) {
-            return this.cep == other.cep
-        }
-        return false
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Endereco
+
+        if (logradouro != other.logradouro) return false
+        if (numero != other.numero) return false
+        if (bairro != other.bairro) return false
+        if (cidade != other.cidade) return false
+        if (estado != other.estado) return false
+        if (cep != other.cep) return false
+        if (complemento != other.complemento) return false
+
+        return true
     }
 
-    override fun hashCode(): Int { // hashCode precisa bater com o equals
-        return cep.hashCode()
+    override fun hashCode(): Int {
+        var result = logradouro.hashCode()
+        result = 31 * result + numero
+        result = 31 * result + bairro.hashCode()
+        result = 31 * result + cidade.hashCode()
+        result = 31 * result + estado.hashCode()
+        result = 31 * result + cep.hashCode()
+        result = 31 * result + complemento.hashCode()
+        return result
     }
+
+
 }
