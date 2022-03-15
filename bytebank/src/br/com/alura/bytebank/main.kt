b7f1c00f93955b2550a19c1defa5d974f1de8929
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-15 11:14:49 -0300

    estudo sobre any - exemplificando a forma como a funcao println deve estar implementeada

--- bytebank/src/br/com/alura/bytebank/main.kt
+++ bytebank/src/br/com/alura/bytebank/main.kt
@@ -1,5 +1,30 @@
-fun main() {
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.Endereco
 
+fun main() {
+    println()
+    println(1)
+    println(1.0)
+    println(true)
+    println("teste")
+
+    val endereco = Endereco()
+
+    println(endereco)
+    println(Cliente())
+
+    imprime()
+    imprime(1)
+    imprime(1.0)
+    imprime(endereco)
 }
+
+fun imprime() {}
+
+fun imprime(valor: Int) {}
+
+fun imprime(valor: Double) {}
+
+fun imprime(valor: Endereco) {}
 
 
