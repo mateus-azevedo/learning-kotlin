1ac2279cbc64b436664c579d9006dd975684f139
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-16 11:13:56 -0300

    explicacao pilha de execucao - usado depurador/debugger

--- bytebank/src/br/com/alura/bytebank/main.kt
+++ bytebank/src/br/com/alura/bytebank/main.kt
@@ -1,5 +1,21 @@
-import br.com.alura.bytebank.teste.testaAny
+package br.com.alura.bytbank
 
 fun main() {
-    testaAny()
+    println("inicio main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1() {
+    println("inicio funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("inicio funcao2")
+    for (i in 1..5) {
+        println(i)
+    }
+    println("fim funcao2")
 }
