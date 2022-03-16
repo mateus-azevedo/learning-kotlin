627778dcd66c982d5566bfddefac0b62b89523c1
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-16 15:15:40 -0300

    codigo formatado kotlin

--- bytebank/src/br/com/alura/bytebank/teste/testaComportamentosConta.kt
+++ bytebank/src/br/com/alura/bytebank/teste/testaComportamentosConta.kt
@@ -12,39 +12,39 @@ fun testaComportamentosConta() {
     val contaFran = ContaPoupanca(titular = fran, numero = 1001)
     contaFran.deposita(300.0)
 
-    println(contaFran.titular);
-    println(contaFran.numero);
-    println(contaFran.saldo);
-    println("======================");
-    println(contaAlex.titular);
-    println(contaAlex.numero);
-    println(contaAlex.saldo);
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+    println("======================")
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
 
-    println("depositando na conta do Alex");
-    contaAlex.deposita(50.0);
-    println(contaAlex.saldo);
+    println("depositando na conta do Alex")
+    contaAlex.deposita(50.0)
+    println(contaAlex.saldo)
 
-    println("depositando na conta da Fran");
-    contaFran.deposita(70.0);
-    println(contaFran.saldo);
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
 
-    println("sacando na conta do Alex");
-    contaAlex.saca(250.0);
-    println(contaAlex.saldo);
+    println("sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
 
-    println("sacando na conta da Fran");
-    contaFran.saca(100.0);
-    println(contaFran.saldo);
+    println("sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
 
-    println("saque em excesso na conta do Alex");
-    contaAlex.saca(100.0);
-    println(contaAlex.saldo);
+    println("saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.saldo)
 
-    println("saque em excesso na conta da Fran");
-    contaFran.saca(500.0);
-    println(contaFran.saldo);
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
 
-    println("transferencia da conta da Fran para o Alex");
+    println("transferencia da conta da Fran para o Alex")
 
     try {
         contaFran.transfere(destino = contaAlex, valor = 300.0)
@@ -55,6 +55,6 @@ fun testaComportamentosConta() {
         e.printStackTrace()
     }
 
-    println("Saldo Alex: R$${contaAlex.saldo}");
-    println("Saldo Fran: R$${contaFran.saldo}");
+    println("Saldo Alex: R$${contaAlex.saldo}")
+    println("Saldo Fran: R$${contaFran.saldo}")
 }
