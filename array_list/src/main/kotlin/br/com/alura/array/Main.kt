63870142c858dae7536c6982a5657bbfd1470aba
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-21 16:23:05 -0300

    criado array de BigDecimal usando generic's

--- array_list/src/main/kotlin/br/com/alura/array/Main.kt
+++ array_list/src/main/kotlin/br/com/alura/array/Main.kt
@@ -1,5 +1,10 @@
 package br.com.alura.array
 
-fun main() {
+import java.math.BigDecimal
 
+fun main() {
+    val salarios = Array<BigDecimal>(5) { BigDecimal.ZERO } // Usando o artificio generic's
+    salarios[0] = "1500.55".toBigDecimal()  // Aqui o valor é convertido de String para BigDecimal. Utilizamos String
+    salarios[1] = "2000.00".toBigDecimal()  // para garantir o valor. Já que Double pode ter problema de arredondamento
+    println(salarios.contentToString())
 }
