32aec6f581310eab165ba78ffbc0f9b0625cd59a
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-21 11:48:56 -0300

    problema da idade com variaveis

---
+++ array_list/src/main/kotlin/br/com/alura/array/Main.kt
@@ -0,0 +1,21 @@
+package br.com.alura.array
+
+fun main() {
+    val idade1 = 25
+    val idade2 = 19
+    val idade3 = 33
+    val idade4 = 20
+
+    val maiorIdade =
+        if (idade1 > idade2 && idade1 > idade3 && idade1 > idade4) {
+            idade1
+        } else if (idade2 > idade3 && idade2 > idade4) {
+            idade2
+        } else if (idade3 > idade4) {
+            idade3
+        } else {
+            idade4
+        }
+
+    println(maiorIdade)
+}
