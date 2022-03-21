163ef009161becb68e807840b310f8a5be701d6c
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-21 12:15:34 -0300

    maior idade verificado por interacao de array

--- array_list/src/main/kotlin/br/com/alura/array/Main.kt
+++ array_list/src/main/kotlin/br/com/alura/array/Main.kt
@@ -1,27 +1,19 @@
 package br.com.alura.array
 
 fun main() {
-    val idade1 = 25
-    val idade2 = 19
-    val idade3 = 33
-    val idade4 = 20
-
-    val maiorIdade =
-        if (idade1 > idade2 && idade1 > idade3 && idade1 > idade4) {
-            idade1
-        } else if (idade2 > idade3 && idade2 > idade4) {
-            idade2
-        } else if (idade3 > idade4) {
-            idade3
-        } else {
-            idade4
-        }
-
-    println(maiorIdade)
-
-    val idades = IntArray(4)
+    val idades = IntArray(5)
     idades[0] = 25
     idades[1] = 19
     idades[2] = 33
     idades[3] = 20
+    idades[4] = 55
+
+    var maiorIdade = 0
+    for (idade in idades) {  // para cada idade dentro de arrays idades executamos uma operação
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+
+    println(maiorIdade)
 }
