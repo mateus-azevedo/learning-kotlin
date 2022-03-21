d3bc44a9ecfe28c242238e204f18bbb7735d41d1
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-21 12:24:13 -0300

    iterando um array com o metodo forEach

--- array_list/src/main/kotlin/br/com/alura/array/Main.kt
+++ array_list/src/main/kotlin/br/com/alura/array/Main.kt
@@ -1,19 +1,23 @@
 package br.com.alura.array
 
 fun main() {
-    val idades = IntArray(5)
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
-    idades[4] = 55
+    val idades: IntArray = intArrayOf(25, 19, 33, 20, 55) // insiro os elementos do array e já atualizo o tamanho do array
 
-    var maiorIdade = 0
+    var maiorIdade = Int.MIN_VALUE  // Garanto que não terá valor menor que este
     for (idade in idades) {  // para cada idade dentro de arrays idades executamos uma operação
         if (idade > maiorIdade) {
             maiorIdade = idade
         }
     }
 
-    println(maiorIdade)
+    println("Maior idade: $maiorIdade")
+
+    var menorIdade = Int.MAX_VALUE  // Garanto que não terá valor maior que este
+    idades.forEach { idade ->
+        if (idade < menorIdade) {
+            menorIdade = idade
+        }
+    }
+
+    println("Menor idade: $menorIdade")
 }
