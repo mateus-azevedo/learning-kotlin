9016cf2c0532e8ed651da3053902115fa7562a35
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-21 12:56:57 -0300

    forEachIndexed unindo o melhor dos dois for in do commit anterior

--- array_list/src/main/kotlin/br/com/alura/array/Main.kt
+++ array_list/src/main/kotlin/br/com/alura/array/Main.kt
@@ -4,21 +4,17 @@ fun main() {
     val salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5000.0, 8000.0, 10000.0)
 
     val aumento = 1.1
-    var indice_array = 0
-    // iterando em cada elemento de salarios
-    for (salario in salarios) {
-        salarios[indice_array] = salario * aumento
-        indice_array++
-    }
-
-    println("Iterado em cada elemento\n${salarios.contentToString()}\n")
-    // resetando valores de salarios p/ o exemplo abaixo
-    salarios[0] = 1500.50; salarios[1] = 2300.0; salarios[2] = 5000.0; salarios[3] = 8000.0; salarios[4] = 10000.0
-
     // iterando em cada indice de salarios
     for (indice in salarios.indices) {
         salarios[indice] = salarios[indice] * aumento
     }
 
     println("Iterado em cada indice\n${salarios.contentToString()}\n")
+
+    // utilizando o metodo forEachIndexed
+    salarios.forEachIndexed { indice, salario ->
+        salarios[indice] = salario * aumento
+    }
+
+    println("Utilizado forEachIndexed\n${salarios.contentToString()}\n")
 }
