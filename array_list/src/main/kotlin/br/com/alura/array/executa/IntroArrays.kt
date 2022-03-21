410016b8b7619166eb2e983e37bbc11a3e4dbad2
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-21 12:33:04 -0300

    IntroArrays.kt separado em um outro arquivo

---
+++ array_list/src/main/kotlin/br/com/alura/array/executa/IntroArrays.kt
@@ -0,0 +1,24 @@
+package br.com.alura.array.executa
+
+fun IntroArrays() {
+    val idades: IntArray =
+        intArrayOf(25, 19, 33, 20, 55) // insiro os elementos do array e já atualizo o tamanho do array
+
+    var maiorIdade = Int.MIN_VALUE  // Garanto que não terá valor menor que este
+    for (idade in idades) {  // para cada idade dentro de arrays idades executamos uma operação
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+
+    println("Maior idade: $maiorIdade")
+
+    var menorIdade = Int.MAX_VALUE  // Garanto que não terá valor maior que este
+    idades.forEach { idade ->
+        if (idade < menorIdade) {
+            menorIdade = idade
+        }
+    }
+
+    println("Menor idade: $menorIdade")
+}
