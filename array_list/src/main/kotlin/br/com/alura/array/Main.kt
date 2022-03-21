ef140f7f147ed848cff911dc4d43fde8def2f970
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-21 16:35:15 -0300

    uso do vararg - uma outra forma de criarmos um Array de BigDecimal

--- array_list/src/main/kotlin/br/com/alura/array/Main.kt
+++ array_list/src/main/kotlin/br/com/alura/array/Main.kt
@@ -3,8 +3,15 @@ package br.com.alura.array
 import java.math.BigDecimal
 
 fun main() {
-    val salarios = Array<BigDecimal>(5) { BigDecimal.ZERO } // Usando o artificio generic's
-    salarios[0] = "1500.55".toBigDecimal()  // Aqui o valor é convertido de String para BigDecimal. Utilizamos String
-    salarios[1] = "2000.00".toBigDecimal()  // para garantir o valor. Já que Double pode ter problema de arredondamento
+    val salarios = bigDecimalArrayOf("1500.55", "2000.00")
+
     println(salarios.contentToString())
+}
+
+// vararg - é um tipo especial do Kotlin que possibilita passagem de argumentos variáveis para uma função
+// Pode passar 1 valor, 5 valores, 100 valores e etc
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
+    return Array<BigDecimal>(valores.size) { index ->
+        valores[index].toBigDecimal()
+    }
 }
