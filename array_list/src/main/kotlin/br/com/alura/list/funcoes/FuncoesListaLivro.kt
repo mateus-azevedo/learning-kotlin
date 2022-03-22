3bb3a28c2f4e7106ea92e16f91b7020b549ccc55
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-22 14:15:49 -0300

    uso do .filterNotNull() - deixando o codigo mais limpo

--- array_list/src/main/kotlin/br/com/alura/list/funcoes/FuncoesListaLivro.kt
+++ array_list/src/main/kotlin/br/com/alura/list/funcoes/FuncoesListaLivro.kt
@@ -4,10 +4,10 @@ import br.com.alura.list.Livro
 
 fun List<Livro?>.imprimeComMarcadores() {
     val tituloFormatado = this
-        .filter { it != null } // faz com que não seja impresso o null
+        .filterNotNull() // método mais recomendado - faz com que não seja impresso o null
         .joinToString(separator = "\n") {
-            " - ${it?.titulo} de ${it?.autor}"
-        }
+            " - ${it.titulo} de ${it.autor}"  // como o .filterNotNull() é um método reconhecido pelo compilador
+        }                                     // não é necessário safe null porque Kotlin entende que não virá null
     println("#### Lista de Livros ####\n$tituloFormatado")
     println()
 }
