ed9738b178b1f8a033f2654170b5a538848cd11a
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-22 11:06:44 -0300

    SalarioComBigDecimal.kt separado em uma outro arquivo

--- array_list/src/main/kotlin/br/com/alura/array/Main.kt
+++ array_list/src/main/kotlin/br/com/alura/array/Main.kt
@@ -1,50 +1,5 @@
 package br.com.alura.array
 
-import br.com.alura.array.funcoes.bigDecimalArrayOf
-import br.com.alura.array.funcoes.media
-import br.com.alura.array.funcoes.somatoria
-import java.math.BigDecimal
-import java.math.RoundingMode
-
 fun main() {
-    val salarios = bigDecimalArrayOf("1500.55", "2000.00", "5000.00", "10000.00")
 
-    println(salarios.contentToString())
-
-    val aumento = "1.1".toBigDecimal()
-    val salariosComAumento: Array<BigDecimal> = salarios
-        .map { salario -> calculaAumentoRelativo(salario, aumento) }
-        .toTypedArray()
-
-    println(salariosComAumento.contentToString())
-
-    val gastoInicial = salariosComAumento.somatoria()
-    println(gastoInicial)
-
-    val meses = 6.toBigDecimal()
-    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salario ->
-        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
-    }
-    println(gastoTotal)
-
-    val mediaTresMaioresSalarios = salariosComAumento
-        .sorted()
-        .takeLast(3)
-        .toTypedArray()
-        .media()
-    println(mediaTresMaioresSalarios)
-
-    val mediaTresMenoresSalarios = salariosComAumento
-        .sorted()
-        .take(3)
-        .toTypedArray()
-        .media()
-    println(mediaTresMenoresSalarios)
 }
-
-private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal): BigDecimal =
-    if (salario < "5000".toBigDecimal()) {
-        salario + "500".toBigDecimal()
-    } else {
-        (salario * aumento).setScale(2, RoundingMode.UP)
-    }
