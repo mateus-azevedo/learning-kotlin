2f281b0f08b8805b52113786eeebe90f3dfa358b
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-22 14:56:01 -0300

    lidando com mutabilidade - variaveis apontam para o mesmo objeto

--- array_list/src/main/kotlin/br/com/alura/list/UsaPrateleira.kt
+++ array_list/src/main/kotlin/br/com/alura/list/UsaPrateleira.kt
@@ -5,6 +5,9 @@ import br.com.alura.list.funcoes.imprimeComMarcadores
 fun main() {
     val prateleira = Prateleira(genero = "Literatura", livros = listaDeLivros)
 
-    prateleira.organizarPorAutor().imprimeComMarcadores()
-    prateleira.organizarPorAnoPublicacao().imprimeComMarcadores()
+    val porAutor = prateleira.organizarPorAutor()
+    val porAnoPublicacao = prateleira.organizarPorAnoPublicacao()
+
+    porAutor.imprimeComMarcadores()             // o resultado da impressão é o mesmo porque ambas variáveis apontam
+    porAnoPublicacao.imprimeComMarcadores()     // para o mesmo objeto. fazendo com que a saida seja a mesma
 }
