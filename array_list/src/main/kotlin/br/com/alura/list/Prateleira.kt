de0356ee72ca7ca9589c40325710952665774c18
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-22 15:10:41 -0300

    lidando com imutabilidade - metodos retornam objetos diferentes (novos)

--- array_list/src/main/kotlin/br/com/alura/list/Prateleira.kt
+++ array_list/src/main/kotlin/br/com/alura/list/Prateleira.kt
@@ -2,15 +2,18 @@ package br.com.alura.list
 
 data class Prateleira (
     val genero: String,
-    val livros: MutableList<Livro>
+    val livros: List<Livro>
 ) {
-    fun organizarPorAutor(): MutableList<Livro> {
-        livros.sortBy { it.autor }
-        return livros
+    /**
+     * O método .sortBy() - ele altera a própria lista, servindo para com os tipos MutableList
+     *
+     * O método .sortedBy() - ele retorna uma nova lista, servindo para com os tipos List
+     * */
+    fun organizarPorAutor(): List<Livro> {
+        return livros.sortedBy { it.autor }
     }
 
-    fun organizarPorAnoPublicacao(): MutableList<Livro> {
-        livros.sortBy { it.anoPublicacao }
-        return livros
+    fun organizarPorAnoPublicacao(): List<Livro> {
+        return livros.sortedBy { it.anoPublicacao }
     }
 }
--- array_list/src/main/kotlin/br/com/alura/list/UsaPrateleira.kt
+++ array_list/src/main/kotlin/br/com/alura/list/UsaPrateleira.kt
@@ -3,11 +3,21 @@ package br.com.alura.list
 import br.com.alura.list.funcoes.imprimeComMarcadores
 
 fun main() {
+    /**
+     * Com as devidas alterações no arquivo Prateleira.kt
+     *
+     * Mesmo livros = listaDeLivros recebendo uma MutableList nossa implementação em Prateleira.kt está para List
+     * e seus métodos estão retornando lista <List> e sabemos que <List> são imutáveis, então, cada retorno de método
+     * eu recebo um objeto diferente resultando na impressão esperada nas linhas 18 e 19 deste arquivo.
+     *
+     *                                     18: porAutor.imprimeComMarcadores()
+     *                                     19: porAnoPublicacao.imprimeComMarcadores()
+     * */
     val prateleira = Prateleira(genero = "Literatura", livros = listaDeLivros)
 
     val porAutor = prateleira.organizarPorAutor()
     val porAnoPublicacao = prateleira.organizarPorAnoPublicacao()
 
-    porAutor.imprimeComMarcadores()             // o resultado da impressão é o mesmo porque ambas variáveis apontam
-    porAnoPublicacao.imprimeComMarcadores()     // para o mesmo objeto. fazendo com que a saida seja a mesma
+    porAutor.imprimeComMarcadores()
+    porAnoPublicacao.imprimeComMarcadores()
 }
