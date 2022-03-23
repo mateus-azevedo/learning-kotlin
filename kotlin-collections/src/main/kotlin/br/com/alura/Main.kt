452d107152b26b240317505f5dcab0121bc9f9e5
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-23 09:35:31 -0300

    outra forma de armazenar valores da lista

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -3,9 +3,7 @@ package br.com.alura
 fun main() {
     val assistiramCursoAndroid: List<String> = listOf("Alex", "Fran", "Gui", "Maria")
     val assistiramCursoKotlin: List<String> = listOf("Alex", "Paulo", "Maria")
-    val assistiramAmbos = mutableListOf<String>()
+    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
 
-    assistiramAmbos.addAll(assistiramCursoAndroid)
-    assistiramAmbos.addAll(assistiramCursoKotlin)
     println(assistiramAmbos)
 }
