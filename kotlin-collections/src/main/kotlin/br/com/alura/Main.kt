dcbebe5e66ff1617b401f52752fb8db0f268f101
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-23 10:41:33 -0300

    comportamentos do set

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -1,9 +1,27 @@
 package br.com.alura
 
 fun main() {
-    val assistiramCursoAndroid: List<String> = listOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin: List<String> = listOf("Alex", "Paulo", "Maria")
-    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+//    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos = mutableSetOf<String>()
 
-    println(assistiramAmbos.distinct())
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
+
+    println(assistiramCursoKotlin + assistiramCursoAndroid)
+    println(assistiramCursoKotlin union assistiramCursoAndroid)
+
+    println(assistiramCursoKotlin - assistiramCursoAndroid)
+    println(assistiramCursoKotlin subtract  assistiramCursoAndroid)
+
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
 }
