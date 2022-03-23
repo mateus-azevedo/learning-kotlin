d1f5e7348e8bea3f32f18714ae1f7a532881f49b
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-23 09:28:20 -0300

    testaCollection.kt separado em arquivo

---
+++ kotlin-collections/src/main/kotlin/br/com/alura/testaCollection.kt
@@ -0,0 +1,45 @@
+package br.com.alura
+
+/**
+ * Iterable - comportamento mais genéricos e limitados
+ * Collection - comportamentos mais específicos em relação ao Iterable
+ * List - comportamentos mais específicos em relação ao Collection
+ *
+ * Veja mais em: https://kotlinlang.org/docs/collections-overview.html
+ * */
+
+fun testaCopia() {
+    val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
+
+    banco.salva("Freeza")
+
+    println(nomesSalvos)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+    val nomes: Collection<String> get() = Companion.dados.toList() // conversão possivel por List ser compátivel com Collections
+
+    fun salva(nome: String) {
+        Companion.dados.add(nome)
+    }
+
+    companion object {
+        /**
+         * Caso trabalhe com qualquer coleção mutável - evite o máximo possível deixá-la exposta
+         * */
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> =
+        mutableListOf("Goku", "Vegeta", "Kuririn", "Trunks", "Piccolo", "Gohan", "Mr.Satan", "Majin Boo", "Deus Yamcha")
+    for (nome in nomes) {
+        println(nome)
+    }
+    println(nomes)
+    println("Tem o nome Alex? ${nomes.contains("Gohan")}")
+    println("Tamanho da coleção ${nomes.size}")
+}
