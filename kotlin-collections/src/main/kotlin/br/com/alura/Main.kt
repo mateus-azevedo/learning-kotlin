72fd58f454cfb993c740ce0e55ed93b49a9d249f
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-25 14:35:09 -0300

    testaMap() em um arquivo separado

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -7,30 +7,7 @@ fun main() {
             Pair(2, 43.0),
             3 to 50.0 // método infix
         )
-    println(pedidos)
-    val pedido = pedidos[1]
-    pedido?.let {
-        println("pedido $it")
+
     }
 
-    for (pedido: Map.Entry<Int, Double> in pedidos) {
-        println("número do pedido: ${pedido.key}")
-        println("valor do pedido ${pedido.value}\n")
-    }
-
-    pedidos[4] = 70.0
-    println(pedidos)
-    pedidos.put(5, 80.0)
-    println(pedidos)
-    pedidos[1] = 100.0
-    println(pedidos)
-    pedidos.putIfAbsent(6, 200.0)
-    println(pedidos)
-    pedidos.putIfAbsent(6, 300.0)
-    println(pedidos)
-
-    pedidos.remove(6)
-    println(pedidos)
-    pedidos.remove(3, 50.0)
-    println(pedidos)
 }
---
+++ kotlin-collections/src/main/kotlin/br/com/alura/testaMap.kt
@@ -0,0 +1,30 @@
+package br.com.alura
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
+    println(pedidos)
+    val pedido = pedidos[1]
+    pedido?.let {
+        println("pedido $it")
+    }
+
+    for (pedido: Map.Entry<Int, Double> in pedidos) {
+        println("número do pedido: ${pedido.key}")
+        println("valor do pedido ${pedido.value}\n")
+    }
+
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5, 80.0)
+    println(pedidos)
+    pedidos[1] = 100.0
+    println(pedidos)
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(3, 50.0)
+    println(pedidos)
+}
