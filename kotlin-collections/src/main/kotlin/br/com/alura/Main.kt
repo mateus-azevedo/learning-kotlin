a22cee1b3f915c52e83f40009ed8ee5794b4c92f
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-25 08:26:59 -0300

    testes utilizando map

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -1,8 +1,8 @@
 package br.com.alura
 
 fun main() {
-    val pedidos =
-        mapOf(
+    val pedidos:MutableMap<Int, Double> =
+        mutableMapOf(
             Pair(1, 20.0),
             Pair(2, 43.0),
             3 to 50.0 // método infix
@@ -17,4 +17,20 @@ fun main() {
         println("número do pedido: ${pedido.key}")
         println("valor do pedido ${pedido.value}\n")
     }
+
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5, 80.0)
+    println(pedidos)
+    pedidos[1] = 100.0
+    println(pedidos)
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(3, 50.0)
+    println(pedidos)
 }
