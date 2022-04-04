b1de3f31e6a927c849ba99c42d9549176a6d6efd
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-04 10:26:53 -0300

    uso do associate

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -1,5 +1,18 @@
 package br.com.alura
 
 fun main() {
-
+    val pedidos:List<Pedido> = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 60.0),
+        Pedido(3, 30.0),
+        Pedido(4, 70.0)
+    )
+    println(pedidos)
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associate { pedido: Pedido ->
+        Pair(pedido.numero, pedido)
+    }
+    println(pedidosMapeados)
+    println(pedidosMapeados[1])
 }
+
+data class Pedido(val numero: Int, val valor: Double)
