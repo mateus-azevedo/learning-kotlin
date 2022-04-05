5ec2048fa6dbcc4dec7c2f47ce6d365c6a08b447
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 10:09:42 -0300

    uso de funcoes no kotlin: funcional e com classe

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -1,5 +1,24 @@
 package br.com.alura
 
+/**
+ * (::) = Referencia da funcao
+ */
 fun main() {
-    testaPedidos()
+    val minhaFuncao: () -> Unit = ::teste
+    println(minhaFuncao)    // imprime a referencia funcao
+    println(minhaFuncao())  // imprime a execucao da funcao (executa a funcao de referencia e imprime o retorno da funcao)
+    println()
+    val minhaFuncaoClasses: () -> Unit = Teste()
+    println(minhaFuncaoClasses)
+    println(minhaFuncaoClasses())
+}
+
+fun teste() {
+    println("exexuta teste")
+}
+
+class Teste: () -> Unit {
+    override fun invoke() {
+        println("executa invoke do Teste")
+    }
 }
