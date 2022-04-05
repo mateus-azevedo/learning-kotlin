0e1f08f511919b3fcaba1e88ce8004e85b706ca3
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 10:36:30 -0300

    uso de funcoes no kotlin: lambda e funcao anonima

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -4,13 +4,33 @@ package br.com.alura
  * (::) = Referencia da funcao
  */
 fun main() {
+//    testaTipoFuncaoReferencia()
+//    println()
+//    testaTipoFuncaoClasse()
+    /**
+     * Estas duas implementações não são reutilizáveis!!
+     * Lambda e Função anônima
+     */
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executa como lambda")
+    }
+    println(minhaFuncaoLambda())
+    val minhaFuncaoAnonima: () -> Unit = fun () {
+        println("Executa funcao anonima")
+    }
+    println(minhaFuncaoAnonima())
+}
+
+private fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasse: () -> Unit = Teste()
+    println(minhaFuncaoClasse)
+    println(minhaFuncaoClasse())
+}
+
+fun testaTipoFuncaoReferencia() {
     val minhaFuncao: () -> Unit = ::teste
     println(minhaFuncao)    // imprime a referencia funcao
     println(minhaFuncao())  // imprime a execucao da funcao (executa a funcao de referencia e imprime o retorno da funcao)
-    println()
-    val minhaFuncaoClasses: () -> Unit = Teste()
-    println(minhaFuncaoClasses)
-    println(minhaFuncaoClasses())
 }
 
 fun teste() {
