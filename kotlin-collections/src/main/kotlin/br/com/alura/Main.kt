e2328216291090218a647a442f2dbcf4f998fa71
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 11:07:41 -0300

    uso de funcoes no kotlin: tipo função com parâmetros e retorno

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -4,41 +4,39 @@ package br.com.alura
  * (::) = Referencia da funcao
  */
 fun main() {
-//    testaTipoFuncaoReferencia()
-//    println()
-//    testaTipoFuncaoClasse()
+    testaTipoFuncaoReferencia()
+    println()
+    testaTipoFuncaoClasse()
     /**
      * Estas duas implementações não são reutilizáveis!!
      * Lambda e Função anônima
      */
-    val minhaFuncaoLambda: () -> Unit = {
-        println("Executa como lambda")
-    }
-    println(minhaFuncaoLambda())
-    val minhaFuncaoAnonima: () -> Unit = fun () {
-        println("Executa funcao anonima")
-    }
-    println(minhaFuncaoAnonima())
+//    val minhaFuncaoLambda: () -> Unit = {
+//        println("Executa como lambda")
+//    }
+//    println(minhaFuncaoLambda())
+//    val minhaFuncaoAnonima: () -> Unit = fun () {
+//        println("Executa funcao anonima")
+//    }
+//    println(minhaFuncaoAnonima())
 }
 
 private fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasse: () -> Unit = Teste()
-    println(minhaFuncaoClasse)
-    println(minhaFuncaoClasse())
+    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClasse(10, 10))
 }
 
 fun testaTipoFuncaoReferencia() {
-    val minhaFuncao: () -> Unit = ::teste
+    val minhaFuncao: (Int, Int) -> Int = ::soma
     println(minhaFuncao)    // imprime a referencia funcao
-    println(minhaFuncao())  // imprime a execucao da funcao (executa a funcao de referencia e imprime o retorno da funcao)
+    println(minhaFuncao(5, 10))  // imprime a execucao da funcao (executa a funcao de referencia e imprime o retorno da funcao)
 }
 
-fun teste() {
-    println("exexuta teste")
+fun soma(a: Int, b:Int): Int {
+    return a + b
 }
 
-class Teste: () -> Unit {
-    override fun invoke() {
-        println("executa invoke do Teste")
-    }
+class Soma: (Int, Int) -> Int {
+    override fun invoke(p1: Int, p2: Int): Int = p1 + p2
+
 }
