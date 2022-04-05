e306e15707de6bf1b7e0eb6109a00d98046b1bb6
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 16:00:10 -0300

    impressao maiusculo com let pt1

--- bytebank/src/br/com/alura/bytebank/main.kt
+++ bytebank/src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,15 @@
 package br.com.alura.bytbank
 
-import br.com.alura.bytebank.teste.testeIntroducaoHigherOrderFunctions
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    testeIntroducaoHigherOrderFunctions()
+    val endereco = Endereco(logradouro = "rua vergueiro", numero = 3185)
+    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    println(enderecoEmMaiusculo)
+
+    val enderecoMaiusculoLet: String = Endereco(logradouro = "rua presidente vargas", numero = 1234)
+        .let { endereco ->
+            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+        }
+    println(enderecoMaiusculoLet)
 }
