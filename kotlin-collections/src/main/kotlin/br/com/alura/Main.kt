e7f07a4929763bc41346692be05ee7ef968b67ef
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 15:21:01 -0300

    testaTipoFuncao.kt separado em um arquivo

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -1,59 +1,5 @@
 package br.com.alura
 
-/**
- * (::) = Referencia da funcao
- */
 fun main() {
-//    testaTipoFuncaoReferencia()
-//    println()
-//    testaTipoFuncaoClasse()
-    /**
-     * Estas duas implementações não são reutilizáveis!!
-     * Lambda e Função anônima
-     */
-    val minhaFuncaoLambda: (Int, Int) -> Int = { a, b -> // quando não usar o parâmetro podemos por _, b
-        a + b
-    }
-    println(minhaFuncaoLambda(15, 10))
-
-    val minhaFuncaoAnonima: (Int, Int) -> Int = fun (a, b): Int {
-        return a + b
-    }
-    println(minhaFuncaoAnonima(20, 10))
-
-    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
-        if (salario > 1000.0) {
-            return@lambda salario + 50
-        }
-        return@lambda salario + 100.0
-    }
-    println(calculaBonificacao(1100.0))
-
-    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
-        if (salario > 1000.0) {
-            return salario + 50
-        }
-        return salario + 100.0
-    }
-    println(calculaBonificacaoAnonima(1100.0))
-}
-
-private fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
-    println(minhaFuncaoClasse(10, 10))
-}
-
-fun testaTipoFuncaoReferencia() {
-    val minhaFuncao: (Int, Int) -> Int = ::soma
-    println(minhaFuncao)    // imprime a referencia funcao
-    println(minhaFuncao(5, 10))  // imprime a execucao da funcao (executa a funcao de referencia e imprime o retorno da funcao)
-}
-
-fun soma(a: Int, b:Int): Int {
-    return a + b
-}
-
-class Soma: (Int, Int) -> Int {
-    override fun invoke(p1: Int, p2: Int): Int = p1 + p2
 
 }
