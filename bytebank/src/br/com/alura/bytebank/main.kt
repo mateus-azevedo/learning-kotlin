020e240fca75f0b6d4f115b8a0119853db00c43a
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 15:24:36 -0300

    criado arquivo testaNullable.kt

--- bytebank/src/br/com/alura/bytebank/main.kt
+++ bytebank/src/br/com/alura/bytebank/main.kt
@@ -1,24 +1,7 @@
 package br.com.alura.bytbank
 
-import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.teste.testaNullable
 
 fun main() {
-    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "casa")
-    val logradouroNovo: String? = enderecoNulo?.logradouro
-
-//    enderecoNulo = null // funciona mesmo com o let
-
-    enderecoNulo?.let {
-        println(it.logradouro.length)
-        val tamanhoComplemento: Int =
-            it.complemento?.length ?: throw IllegalStateException("Complemento nao pode ser vazio") // Elvis Operator - syntax sugar
-        println(tamanhoComplemento)
-    }
-    teste("")
-    teste(1)
-}
-
-fun teste(valor: Any) {
-    val numero: Int? = valor as? Int // safecasts
-    println(numero)
+    testaNullable()
 }
---
+++ bytebank/src/br/com/alura/bytebank/teste/testaNullable.kt
@@ -0,0 +1,25 @@
+package br.com.alura.bytebank.teste
+
+import br.com.alura.bytebank.modelo.Endereco
+
+fun testaNullable() {
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "casa")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+
+//    enderecoNulo = null // funciona mesmo com o let
+
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length
+                ?: throw IllegalStateException("Complemento nao pode ser vazio") // Elvis Operator - syntax sugar
+        println(tamanhoComplemento)
+    }
+    teste("")
+    teste(1)
+}
+
+fun teste(valor: Any) {
+    val numero: Int? = valor as? Int // safecasts
+    println(numero)
+}
