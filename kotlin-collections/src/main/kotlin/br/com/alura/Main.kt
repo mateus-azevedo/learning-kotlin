6c50efee6803c8fd6dc25dc2e2394a3106e6f9b5
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 12:16:39 -0300

    implementado calculaBonificacao com funcao anonima

--- kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
+++ kotlin-collections/src/main/kotlin/br/com/alura/Main.kt
@@ -28,6 +28,14 @@ fun main() {
         return@lambda salario + 100.0
     }
     println(calculaBonificacao(1100.0))
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if (salario > 1000.0) {
+            return salario + 50
+        }
+        return salario + 100.0
+    }
+    println(calculaBonificacaoAnonima(1100.0))
 }
 
 private fun testaTipoFuncaoClasse() {
