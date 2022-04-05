1d675adc799106e99102c6db4eef3177eb343477
Mateus Santos <m.santos.azeve@gmail.com>
2022-04-05 15:45:24 -0300

    conhecendo as funcoes de alta ordem - Higher-Order Functions

--- bytebank/src/br/com/alura/bytebank/main.kt
+++ bytebank/src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,27 @@
 package br.com.alura.bytbank
 
-import br.com.alura.bytebank.teste.testaNullable
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    testaNullable()
+    /**
+     * Higher Order Function
+     * Quando uma função recebe uma outra função ou retorna uma função
+     */
+    val testeFuncao: () -> Unit
+    Endereco().let {
+        it
+    }
+    "".let(::testeRecebeString)
+    1.let {
+        it
+    }
+    teste(1, {})
+}
+
+fun testeRecebeString(valor: String) {
+
+}
+
+fun teste(teste: Int, bloco: () -> Unit): () -> Unit {
+    return {}
 }
